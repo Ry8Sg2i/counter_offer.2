75ea6a689d3f62555e48438d696f97115c446b79
Ryu <azatose8684@gmail.com>
2023-12-27 08:34:49 +0900

    color変更ページ作成

---
+++ lib/src/screen/setting/accoumt/colorswich.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:counterofferv1/provider/river1.dart';
+
+// 1. ConsumerWidgetを継承する
+class Colorswich extends ConsumerWidget {
+  const Colorswich({Key? key}) : super(key: key);
+
+  @override
+  // 2. 引数に WidgetRef ref を取る 
+  Widget build(BuildContext context, WidgetRef ref) {
+    // ref.watch() を宣言する
+    final themeMode = ref.read(themeModeProvider.notifier);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Brightness Demo"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: PopupMenuButton<ThemeMode>(
+              icon: const Icon(Icons.settings_brightness,),
+              // themeMode.state に選択された 外観モード をセットする
+              onSelected: (ThemeMode selectedThemeMode) => themeMode.state = selectedThemeMode,
+              itemBuilder: (context) => <PopupMenuEntry<ThemeMode>>[
+                const PopupMenuItem(
+                  value: ThemeMode.system,
+                  child: Text('システム設定に従う'),
+                ),
+                const PopupMenuItem(
+                  value: ThemeMode.light, 
+                  child: Text('ライトモード'),
+                ),
+                const PopupMenuItem(
+                  value: ThemeMode.dark,
+                  child: Text('ダークモード'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          "設定に応じてfontColorが入れ替わる", 
+          style: TextStyle(fontSize: 30.0),
+        ),
+      ),
+    );
+  }
+}
+
