e8ac5ba6d1b8310cdf05d0de33bf6fbf6a8c20fd
Ryu <azatose8684@gmail.com>
2024-01-04 00:00:00 +0900

    ログイン状態保持試作

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:counterofferv1/auth/login_page.dart';
 import 'package:counterofferv1/colors/color_schemes.g.dart';
 import 'package:counterofferv1/route/router.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:counterofferv1/provider/river1.dart';
 
 
 void main()  async {
@@ -39,6 +41,22 @@ class CounterOffer extends ConsumerWidget {
           ),
         ),
         routerConfig: goRouter,
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // StreamProvider を監視し、AsyncValue<User?> を取得する。
+    final authStateAsync = ref.watch(authStateChangesProvider);
+    // パターンマッチングを使用して、状態をUIにマッピングする
+    return authStateAsync.when(
+      data: (user) => user != null ? const HomePage() : const LoginPage(),
+      loading: () => const CircularProgressIndicator(),
+      error: (err, stack) => Text('Error: $err'),
     );
   }
 }
