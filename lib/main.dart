7668a1ad4b758daa3b9083c0abbb8422e14cf1bb
Ryu <azatose8684@gmail.com>
2024-01-30 10:24:51 +0900

    del

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:counterofferv1/auth/login_page.dart';
 import 'package:counterofferv1/colors/color_schemes.g.dart';
 import 'package:counterofferv1/route/router.dart';
 import 'package:flutter/material.dart';
@@ -6,8 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:counterofferv1/provider/river1.dart';
-
 
 void main()  async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -41,22 +38,6 @@ class CounterOffer extends ConsumerWidget {
           ),
         ),
         routerConfig: goRouter,
-    );
-  }
-}
-
-class HomePage extends ConsumerWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // StreamProvider を監視し、AsyncValue<User?> を取得する。
-    final authStateAsync = ref.watch(authStateChangesProvider);
-    // パターンマッチングを使用して、状態をUIにマッピングする
-    return authStateAsync.when(
-      data: (user) => user != null ? const HomePage() : const LoginPage(),
-      loading: () => const CircularProgressIndicator(),
-      error: (err, stack) => Text('Error: $err'),
     );
   }
 }
