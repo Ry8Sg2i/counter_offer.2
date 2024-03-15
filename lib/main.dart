96ad41874beb634f604ba1ce5a9735242c322e17
Ryu <azatose8684@gmail.com>
2024-03-16 00:00:00 +0900

    青エラー対処法変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:counterofferv1/colors/color_schemes.g.dart';
 import 'package:counterofferv1/route/router.dart';
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
