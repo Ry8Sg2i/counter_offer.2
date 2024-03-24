8aef9fc8311722e7cf1af5bce9c374c4c083fd93
Ryu <azatose8684@gmail.com>
2024-03-24 19:04:59 +0900

    UpperCamelCaseスタイルに変更

--- lib/src/screen/settingpage/accoumt/account.dart
+++ lib/src/screen/settingpage/accoumt/account.dart
@@ -1,11 +1,10 @@
-// ignore_for_file: camel_case_types
 import 'package:counterofferv1/compornents/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class accountSetting extends ConsumerWidget {
-  const accountSetting({super.key});
+class AccountSetting extends ConsumerWidget {
+  const AccountSetting({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
